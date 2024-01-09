0933b2e0a196b5bf9507db482c0560fee86be6b1
Arthur Gartner <Arthur.Gartner@gmail.com>
2024-01-08 21:19:52 -0700

    Finished tutorial

--- remix-course/app/routes/notes._index.jsx
+++ remix-course/app/routes/notes._index.jsx
@@ -2,22 +2,19 @@ import { json, redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "../components/NewNote";
 import NoteList, { links as noteListLinks } from "../components/NoteList";
 import { getStoredNotes, storeNotes } from "../data/notes";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useRouteError } from "@remix-run/react";
 
 export default function NotesPage() {
   const notes = useLoaderData();
 
   return (
-    <>
-      <main>
-        <NewNote />
-        <NoteList notes={notes} />
-      </main>
-    </>
+    <main>
+      <NewNote />
+      <NoteList notes={notes} />
+    </main>
   );
 }
 
-//Backend code for get requests
 export async function loader() {
   const notes = await getStoredNotes();
   if (!notes || notes.length === 0) {
@@ -25,30 +22,26 @@ export async function loader() {
       { message: "Could not find any notes." },
       {
         status: 404,
+        statusText: "Not Found",
       }
     );
   }
   return notes;
 }
 
-//Backend code
 export async function action({ request }) {
   const formData = await request.formData();
-  // const noteData = {
-  //     title: formData.get('title'),
-  //     content: formData.get('content')
-  // }
   const noteData = Object.fromEntries(formData);
+
   if (noteData.title.trim().length < 5) {
-    return { message: "Invalid title - must be atleast 5 characters long." };
+    return { message: "Invalid title - must be at least 5 characters long." };
   }
+
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
-
   const updatedNotes = existingNotes.concat(noteData);
-
   await storeNotes(updatedNotes);
-
+  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
   return redirect("/notes");
 }
 
@@ -56,4 +49,27 @@ export function links() {
   return [...newNoteLinks(), ...noteListLinks()];
 }
 
-export function CatchBoundary() {}
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+
+  const message = caughtResponse.data?.message || "Data not found.";
+
+  return (
+    <main>
+      <NewNote />
+      <p className="info-message">{message}</p>
+    </main>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <main className="error">
+      <h1>An error related to your notes occurred!</h1>
+      <p>{error.message}</p>
+      <p>
+        Back to <Link to="/">safety</Link>!
+      </p>
+    </main>
+  );
+}
